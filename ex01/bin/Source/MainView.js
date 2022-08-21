d993a9292652f989fc08c091c3fed5f2ab9657d7
이정우 <ljw8124@gmail.com>
2022-08-22 08:54:46 +0900

    CDN 방식

--- ex01/bin/Source/MainView.js
+++ ex01/bin/Source/MainView.js
@@ -20,15 +20,11 @@ MainView.prototype.init = function(context, evtListener)
 	this.tabView.addTab('ex01', 'Source/ex01/ex01.lay', 'ex01');
 	this.tabView.addTab('ex02', 'Source/ex02/ex02.lay', 'ex02');
 	this.tabView.addTab('ex03', 'Source/ex03/ex03.lay', 'ex03');
-
 };
 
 MainView.prototype.onInitDone = function()
 {
-	AView.prototype.onInitDone.call(this);
-
-	//TODO:edit here
-
+	this.tabView.selectTabByIndex(0);
 };
 
 MainView.prototype.onActiveDone = function(isFirst)
@@ -36,30 +32,5 @@ MainView.prototype.onActiveDone = function(isFirst)
 	AView.prototype.onActiveDone.call(this, isFirst);
 
 	//TODO:edit here
-
-};
-
-MainView.prototype.onToBtnClick = function(comp, info, e)
-{
-	this.tabView.selectTabById(comp.compId);
-
-};
-
-MainView.prototype.onHideBtnClick = function(comp, info, e)
-{
-	var btn = this.hideBtn;
-	var btnGroup = this.headerView.findCompByGroup('btn');
-	
-	if(btn.getText() == 'HIDE') {
-		btn.setText('SHOW');
-		btnGroup.forEach(btn => {
-			btn.hide();
-		});
-	} else {
-		btn.setText('HIDE');
-		btnGroup.forEach(btn => {
-			btn.show();
-		});
-	}
 
 };
